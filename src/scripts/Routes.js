b4ce5b8c7be02ec16f0a1d215c82c29fd96a6a52
Tales de Lima Filho <highhard2012@gmail.com>
2024-08-11 10:03:26 -0300

    getJogadores

---
+++ src/scripts/Routes.js
@@ -0,0 +1,34 @@
+const { default: axios } = require("axios");
+
+export function getJogadores() {
+  let jogadores = [];
+
+  let config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: "https://api-football-v1.p.rapidapi.com/v3/players?league=39&season=2020",
+    headers: {
+      "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+      "X-RapidApi-Key": "5fbfa5f499mshaf2a891b46c2ddap1b9f3ejsne3b6245a8e75",
+    },
+  };
+
+  axios
+    .request(config)
+    .then(({ response }) => {
+      response.forEach((dadosJogador) => {
+        jogadores.push({
+          id: dadosJogador.player.id,
+          imagem: dadosJogador.player.photo,
+          name: dadosJogador.player.name,
+          time: dadosJogador.statistics[0].team.name,
+          position: dadosJogador.statistics[0].games.position,
+        });
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  return { jogadores };
+}
