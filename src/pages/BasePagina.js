f826b6f93bf9df8769342ca2ec29486fd1613e23
Tales de Lima Filho <highhard2012@gmail.com>
2024-08-17 14:31:03 -0300

    Base pagina base, aguardando adicao da barra lateral

--- src/pages/BasePagina.js
+++ src/pages/BasePagina.js
@@ -1,17 +1,25 @@
 import { useState } from "react";
 import PlayerCard from "../components/CardJogador";
+import InputPesquisa from "../components/InputPesquisa";
+import Listagem from "../components/Listagem";
+import { mock } from "../scripts/mockedResponse";
 
 //Página para teste de componentes e criação da base para a aba de favoritos e atletas
 export default function BasePagina() {
-  const [jogadores, setJogadores] = useState({
-    jogadores: [{}],
-  });
+  const [jogadores, setJogadores] = useState(mock);
 
   return (
     <>
-      {players.jogadores.map((jogador) => {
-        return <PlayerCard jogador={jogador} />;
-      })}
+      <div className="mt-10">
+        <Listagem
+          jogadores={jogadores}
+          textoPesquisado={"Capote"}
+          titulo={"Atletas"}
+        />
+      </div>
+      <div className="fixed top-5 right-5">
+        <InputPesquisa />
+      </div>
     </>
   );
 }
