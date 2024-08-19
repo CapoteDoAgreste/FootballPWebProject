a479653cf1c992df7c3640797034dd26c8a47e8e
Tales de Lima Filho <highhard2012@gmail.com>
2024-08-19 18:50:51 -0300

    Script favoritos e adequações nos componentes para permitir o funcionamento

--- src/pages/BasePagina.js
+++ src/pages/BasePagina.js
@@ -5,7 +5,9 @@ import { mock } from "../scripts/mockedResponse";
 import { getJogadores } from "../scripts/Routes";
 
 export default function BasePagina({ titulo }) {
-  const [jogadores, setJogadores] = useState(mock);
+  const [jogadores, setJogadores] = useState(
+    JSON.parse(localStorage.getItem("favoritos"))
+  );
   const [lista, setLista] = useState(jogadores);
   const [pesquisa, setPesquisa] = useState("");
 
@@ -13,17 +15,84 @@ export default function BasePagina({ titulo }) {
     const fetchJogadores = async () => {
       const fetchedJogadores = await getJogadores();
       setJogadores(fetchedJogadores);
-      setLista(jogadores);
+      setLista(fetchedJogadores);
     };
-    if (titulo == "Atletas") {
+
+    if (titulo === "Atletas") {
       fetchJogadores();
+    } else {
+      getFavoritos();
     }
-  }, []);
+  }, [titulo]);
+
+  const getFavoritos = () => {
+    const favoritos = JSON.parse(localStorage.getItem("favoritos"));
+    if (favoritos?.jogadores?.length > 0) {
+      if (titulo !== "Atletas") {
+        setJogadores(favoritos);
+        setLista(jogadores);
+      }
+    } else {
+      criarFavorito();
+    }
+  };
+
+  const criarFavorito = (novoFavorito) => {
+    let favoritoData = { jogadores: [] };
+    if (novoFavorito) {
+      favoritoData.jogadores?.push(novoFavorito);
+    }
+    localStorage.setItem("favoritos", JSON.stringify(favoritoData));
+    if (titulo !== "Atletas") {
+      setJogadores(favoritoData);
+      setLista(jogadores);
+    }
+  };
+
+  const adicionarFavorito = (novoFavorito, setImagem, imagem) => {
+    const favoritos = JSON.parse(localStorage.getItem("favoritos")) || {
+      jogadores: [],
+    };
+
+    const existeFavorito = favoritos?.jogadores.some(
+      (jogador) => jogador.id === novoFavorito.id
+    );
+
+    if (!existeFavorito) {
+      favoritos.jogadores.push(novoFavorito);
+      localStorage.setItem("favoritos", JSON.stringify(favoritos));
+      if (titulo !== "Atletas") {
+        setJogadores(favoritos);
+        setLista(favoritos.jogadores);
+      }
+      setImagem(imagem);
+    } else {
+      console.log("O jogador já está na lista de favoritos.");
+    }
+  };
+
+  const tirarFavorito = (jogador, setImagem, imagem) => {
+    const favoritos = JSON.parse(localStorage.getItem("favoritos")) || {
+      jogadores: [],
+    };
+
+    const indiceFavorito = favoritos.jogadores.findIndex(
+      (jogador) => jogador.id === jogador.id
+    );
+
+    if (indiceFavorito !== -1) {
+      favoritos.jogadores.splice(indiceFavorito, 1);
+      localStorage.setItem("favoritos", JSON.stringify(favoritos));
+      setImagem(imagem);
+    } else {
+      console.log("O jogador não está na lista de favoritos.");
+    }
+  };
 
   const search = (pesquisaString) => {
     setPesquisa(pesquisaString);
     setLista({
-      jogadores: jogadores.jogadores.filter((jogador) =>
+      jogadores: jogadores?.jogadores?.filter((jogador) =>
         jogador.nome.toLowerCase().includes(pesquisaString.toLowerCase())
       ),
     });
@@ -36,6 +105,8 @@ export default function BasePagina({ titulo }) {
           jogadores={lista}
           textoPesquisado={pesquisa}
           titulo={titulo}
+          adicionarFavorito={adicionarFavorito}
+          tirarFavorito={tirarFavorito}
         />
       </div>
       <div className="fixed top-5 right-5">
